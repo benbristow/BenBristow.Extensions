a2bf5acd11257d6e66b2468368ab282de7ea9d4a
Ben Bristow <ben@bristow.me>
2024-07-05 19:27:05 +0100

    doc: add <summary> docs to WhereIf method

--- BenBristow.Extensions/EnumerableExtensions.cs
+++ BenBristow.Extensions/EnumerableExtensions.cs
@@ -2,6 +2,14 @@ namespace BenBristow.Extensions;
 
 public static class EnumerableExtensions
 {
+    /// <summary>
+    /// Filters a sequence of values based on a predicate if a given condition is true.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> to filter.</param>
+    /// <param name="condition">A boolean condition that determines whether the predicate should be applied.</param>
+    /// <param name="predicate">A function to test each element for a condition.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> that contains elements from the input sequence that satisfy the condition if <paramref name="condition"/> is true; otherwise, the original sequence.</returns>
     public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, bool> predicate) =>
         condition ? source.Where(predicate) : source;
 }
--- BenBristow.Extensions/QueryableExtensions.cs
+++ BenBristow.Extensions/QueryableExtensions.cs
@@ -4,6 +4,14 @@ namespace BenBristow.Extensions;
 
 public static class QueryableExtensions
 {
+    /// <summary>
+    /// Applies a predicate to filter a sequence of values based on a specified condition.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The sequence to apply the predicate to.</param>
+    /// <param name="condition">A boolean value that determines whether the predicate should be applied.</param>
+    /// <param name="predicate">A function to test each element for a condition. Only invoked if <paramref name="condition"/> is true.</param>
+    /// <returns>An <see cref="IQueryable{T}"/> that contains elements from the input sequence that satisfy the condition if <paramref name="condition"/> is true; otherwise, returns the original sequence.</returns>
     public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Expression<Func<T, bool>> predicate) =>
         condition ? source.Where(predicate) : source;
 }
